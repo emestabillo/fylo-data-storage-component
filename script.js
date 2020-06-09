ed66c0fb712233346279ff87f601fc5bc6905b47
Emmilie Estabillo <emsestabillo@gmail.com>
2020-06-08 20:47:46 -0400

    Adjust js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// const counters = document.querySelectorAll(".counter");
-// const speed = 150;
+const counters = document.querySelectorAll(".counter");
+const speed = 150;
 
-// counters.forEach((counter) => {
-//   const updateCount = () => {
-//     const target = +counter.getAttribute("data-target");
-//     const count = +counter.innerText;
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
 
-//     const increment = target / speed;
+    const increment = target / speed;
 
-//     if (count < target) {
-//       counter.innerText = Math.min(Math.ceil(count + increment), target);
-//       setTimeout(updateCount, 1);
-//     } else {
-//       count.innerText = target;
-//     }
-//   };
-//   updateCount();
-// });
+    if (count < target) {
+      counter.innerText = Math.min(Math.ceil(count + increment), target);
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
